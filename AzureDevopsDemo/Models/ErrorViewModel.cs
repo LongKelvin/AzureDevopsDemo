8e3e25cb630079ae410fcd401467099767c912a4
LongKelvin <longkelvin101099@gmail.com>
2022-11-15 10:37:19 +0700

    Initial commit

---
+++ AzureDevopsDemo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AzureDevopsDemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
