44aea64931a1e2595f64284e7a03a91e42a37b54
Long Kelvin <longkelvin101099@gmail.com>
2022-11-15 14:12:41 +0700

    Update test case

--- AzureDevopsDemo.NUnitTest/TestGlobal.cs
+++ AzureDevopsDemo.NUnitTest/TestGlobal.cs
@@ -8,9 +8,40 @@ namespace AzureDevopsDemo.NUnitTest
         }
 
         [Test]
-        public void TestGlobal()
+        public void TestGlobal_Pass()
         {
             Assert.Pass();
+        }
+        [Test]
+        public void TestGlobal_Pass1()
+        {
+            Assert.Pass();
+        }
+        [Test]
+        public void TestGlobal_Pass2()
+        {
+            Assert.Pass();
+        }
+        [Test]
+        public void TestGlobal_Pass3()
+        {
+            Assert.Pass();
+        }
+        [Test]
+        public void TestGlobal_Pass4()
+        {
+            Assert.Pass();
+        }
+        [Test]
+        public void TestGlobal_Pass5()
+        {
+            Assert.Pass();
+        }
+
+        [Test]
+        public void TestGlobal_Failed() 
+        {
+            Assert.Fail();
         }
     }
 }
